0566dbc8ef20ab745f73256b93ba3fa4aae811ae
Kássia Catarine <kassia_catarine.15@hotmail.com>
2019-07-08 18:25:55 -0300

    Inicializando projeto
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

---
+++ src/main/java/br/edu/utfpr/asreplacement/AsReplacementApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.asreplacement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsReplacementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsReplacementApplication.class, args);
+	}
+
+}
